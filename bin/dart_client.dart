e7b3335e0b6dd7974452655ec27f814f0e523a41
Jorge Sardina <jorge@skilldevs.com>
2023-03-28 18:07:59 +0200

    Initial commit

---
+++ bin/dart_client.dart
@@ -0,0 +1,5 @@
+import 'package:dart_client/dart_client.dart' as dart_client;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_client.calculate()}!');
+}
---
+++ test/dart_client_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_client/dart_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
