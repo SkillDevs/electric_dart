4967c3a7313b9975dd5eb1dab2ae5431f88fc420
Jorge Sardina <jorge@skilldevs.com>
2023-05-31 18:56:36 +0200

    cleanup

--- lib/src/util/hex.dart
+++ lib/src/util/hex.dart
@@ -1,11 +1,6 @@
 String toHexString(List<int> byteArray) {
-  return byteArray.fold('', (acc, byte)  {
-    final String part = '0' + (byte & 0xff).toRadixString(16);
-
-    // TODO: Remove this print
-    print("HEX: $byte -> $part");
-
-    final sliced = part.substring(part.length - 2);
-    return acc + sliced;
+  return byteArray.fold('', (acc, byte) {
+    final String part = (byte & 0xff).toRadixString(16).padLeft(2, "0");
+    return acc + part;
   });
 }
