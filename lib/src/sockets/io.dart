ffa561cb0d2f3a5bae834a262854a7667ac6b1d8
David Martos <davidmartos96@gmail.com>
2023-07-14 16:49:21 +0200

    generalize sockets implementation for io and html

--- lib/src/sockets/io.dart
+++ lib/src/sockets/io.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
 import 'dart:io' as io;
-import 'dart:typed_data';
 
 import 'package:electric_client/src/sockets/sockets.dart';
 import 'package:web_socket_channel/io.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 SocketFactory getDefaultSocketFactory() {
   return WebSocketIOFactory();
@@ -16,134 +16,18 @@ class WebSocketIOFactory implements SocketFactory {
   }
 }
 
-class WebSocketIO implements Socket {
-  IOWebSocketChannel? _channel;
-  List<StreamSubscription<dynamic>> _subscriptions = [];
-
-  List<void Function()> _onceConnectCallbacks = [];
-  List<void Function(Object error)> _onceErrorCallbacks = [];
-
-  List<void Function(Object error)> _errorCallbacks = [];
-  List<void Function()> _closeCallbacks = [];
-  List<void Function(Data data)> _messageCallbacks = [];
-
+class WebSocketIO extends WebSocketBase<io.WebSocket> {
   @override
-  Socket closeAndRemoveListeners() {
-    _socketClose();
-    _onceConnectCallbacks = [];
-    _onceErrorCallbacks = [];
-
-    return this;
-  }
-
-  void _socketClose() {
-    for (var subscription in _subscriptions) {
-      subscription.cancel();
-    }
-    _subscriptions = [];
-
-    _channel?.sink.close();
-
-    final closeCallbacks = [..._closeCallbacks];
-    _closeCallbacks = [];
-    for (final cb in closeCallbacks) {
-      cb();
-    }
-
-    _errorCallbacks = [];
-    _messageCallbacks = [];
-
-    _channel = null;
-  }
-
-  @override
-  void onClose(void Function() cb) {
-    _closeCallbacks.add(cb);
-  }
-
-  @override
-  void onError(void Function(Object error) cb) {
-    _errorCallbacks.add(cb);
-  }
-
-  @override
-  void onMessage(void Function(Data data) cb) {
-    _messageCallbacks.add(cb);
-  }
-
-  @override
-  void onceConnect(void Function() cb) {
-    _onceConnectCallbacks.add(cb);
-  }
-
-  @override
-  void onceError(void Function(Object error) cb) {
-    _onceErrorCallbacks.add(cb);
-  }
-
-  @override
-  Socket open(ConnectionOptions opts) {
-    _asyncStart(opts);
-    return this;
-  }
-
-  Future<void> _asyncStart(ConnectionOptions opts) async {
-    late final io.WebSocket ws;
-    try {
-      ws = await io.WebSocket.connect(
-        opts.url,
-      );
-    } catch (e) {
-      _notifyErrorAndCloseSocket(Exception('failed to establish connection'));
-      return;
-    }
-
-    // Notify connected
-    while (_onceConnectCallbacks.isNotEmpty) {
-      _onceConnectCallbacks.removeLast()();
-    }
-
-    _channel = IOWebSocketChannel(ws);
-    final msgSubscription = _channel!.stream //
-        .listen(
-      (rawData) {
-        try {
-          final bytes = rawData as Uint8List;
-
-          // Notify message
-          for (final cb in _messageCallbacks) {
-            cb(bytes);
-          }
-        } catch (e) {
-          _notifyErrorAndCloseSocket(e);
-        }
-      },
-      cancelOnError: true,
-      onError: (Object e) {
-        _notifyErrorAndCloseSocket(e);
-      },
-      onDone: () {
-        _socketClose();
-      },
+  Future<io.WebSocket> createNativeSocketConnection(String url) async {
+    final ws = await io.WebSocket.connect(
+      url,
     );
-    _subscriptions.add(msgSubscription);
-  }
 
-  void _notifyErrorAndCloseSocket(Object e) {
-    for (final cb in _errorCallbacks) {
-      cb(e);
-    }
-
-    while (_onceErrorCallbacks.isNotEmpty) {
-      _onceErrorCallbacks.removeLast()(e);
-    }
-
-    _socketClose();
+    return ws;
   }
 
   @override
-  Socket write(Data data) {
-    _channel?.sink.add(data);
-    return this;
+  WebSocketChannel createSocketChannel(io.WebSocket socketType) {
+    return IOWebSocketChannel(socketType);
   }
 }
--- lib/src/sockets/sockets.dart
+++ lib/src/sockets/sockets.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
 import 'dart:typed_data';
+
+import 'package:meta/meta.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 export 'stub.dart'
     if (dart.library.io) 'io.dart'
@@ -27,4 +31,142 @@ class ConnectionOptions {
 
 abstract class SocketFactory {
   Socket create();
+}
+
+/// Socket implementation that uses web_socket_channel
+/// io and html both derive from the main logic here
+abstract class WebSocketBase<SocketType> implements Socket {
+  WebSocketChannel? _channel;
+  List<StreamSubscription<dynamic>> _subscriptions = [];
+
+  List<void Function()> _onceConnectCallbacks = [];
+  List<void Function(Object error)> _onceErrorCallbacks = [];
+
+  List<void Function(Object error)> _errorCallbacks = [];
+  List<void Function()> _closeCallbacks = [];
+  List<void Function(Data data)> _messageCallbacks = [];
+
+  @protected
+  Future<SocketType> createNativeSocketConnection(String url);
+
+  @protected
+  WebSocketChannel createSocketChannel(SocketType socketType);
+
+  @override
+  Socket closeAndRemoveListeners() {
+    _socketClose();
+    _onceConnectCallbacks = [];
+    _onceErrorCallbacks = [];
+
+    return this;
+  }
+
+  void _socketClose() {
+    for (var subscription in _subscriptions) {
+      subscription.cancel();
+    }
+    _subscriptions = [];
+
+    _channel?.sink.close();
+
+    final closeCallbacks = [..._closeCallbacks];
+    _closeCallbacks = [];
+    for (final cb in closeCallbacks) {
+      cb();
+    }
+
+    _errorCallbacks = [];
+    _messageCallbacks = [];
+
+    _channel = null;
+  }
+
+  @override
+  void onClose(void Function() cb) {
+    _closeCallbacks.add(cb);
+  }
+
+  @override
+  void onError(void Function(Object error) cb) {
+    _errorCallbacks.add(cb);
+  }
+
+  @override
+  void onMessage(void Function(Data data) cb) {
+    _messageCallbacks.add(cb);
+  }
+
+  @override
+  void onceConnect(void Function() cb) {
+    _onceConnectCallbacks.add(cb);
+  }
+
+  @override
+  void onceError(void Function(Object error) cb) {
+    _onceErrorCallbacks.add(cb);
+  }
+
+  @override
+  Socket open(ConnectionOptions opts) {
+    _asyncStart(opts);
+    return this;
+  }
+
+  Future<void> _asyncStart(ConnectionOptions opts) async {
+    late final SocketType ws;
+    try {
+      ws = await createNativeSocketConnection(opts.url);
+    } catch (e) {
+      _notifyErrorAndCloseSocket(Exception('failed to establish connection'));
+      return;
+    }
+
+    // Notify connected
+    while (_onceConnectCallbacks.isNotEmpty) {
+      _onceConnectCallbacks.removeLast()();
+    }
+
+    _channel = createSocketChannel(ws);
+    final msgSubscription = _channel!.stream //
+        .listen(
+      (rawData) {
+        try {
+          final bytes = rawData as Uint8List;
+
+          // Notify message
+          for (final cb in _messageCallbacks) {
+            cb(bytes);
+          }
+        } catch (e) {
+          _notifyErrorAndCloseSocket(e);
+        }
+      },
+      cancelOnError: true,
+      onError: (Object e) {
+        _notifyErrorAndCloseSocket(e);
+      },
+      onDone: () {
+        _socketClose();
+      },
+    );
+    _subscriptions.add(msgSubscription);
+  }
+
+  void _notifyErrorAndCloseSocket(Object e) {
+    for (final cb in _errorCallbacks) {
+      cb(e);
+    }
+
+    while (_onceErrorCallbacks.isNotEmpty) {
+      _onceErrorCallbacks.removeLast()(e);
+    }
+
+    _socketClose();
+  }
+
+  @override
+  Socket write(Data data) {
+    _channel?.sink.add(data);
+    return this;
+  }
 }
