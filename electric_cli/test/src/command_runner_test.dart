cf71001e0455e76957fb5d6db402783bf0c8a019
David Martos <davidmartos96@gmail.com>
2023-07-20 10:30:19 +0200

    CLI boilerplate

---
+++ electric_cli/test/src/command_runner_test.dart
@@ -0,0 +1,103 @@
+import 'package:args/command_runner.dart';
+import 'package:cli_completion/cli_completion.dart';
+import 'package:electric_cli/src/command_runner.dart';
+import 'package:electric_cli/src/version.dart';
+import 'package:mason_logger/mason_logger.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:test/test.dart';
+
+class _MockLogger extends Mock implements Logger {}
+
+const latestVersion = '0.0.0';
+
+final updatePrompt = '''
+${lightYellow.wrap('Update available!')} ${lightCyan.wrap(packageVersion)} \u2192 ${lightCyan.wrap(latestVersion)}
+Run ${lightCyan.wrap('$executableName update')} to update''';
+
+void main() {
+  group('ElectricCliCommandRunner', () {
+    late Logger logger;
+    late ElectricCliCommandRunner commandRunner;
+
+    setUp(() {
+      logger = _MockLogger();
+
+      commandRunner = ElectricCliCommandRunner(
+        logger: logger,
+      );
+    });
+
+    test('can be instantiated without an explicit analytics/logger instance',
+        () {
+      final commandRunner = ElectricCliCommandRunner();
+      expect(commandRunner, isNotNull);
+      expect(commandRunner, isA<CompletionCommandRunner<int>>());
+    });
+
+    test('handles FormatException', () async {
+      const exception = FormatException('oops!');
+      var isFirstInvocation = true;
+      when(() => logger.info(any())).thenAnswer((_) {
+        if (isFirstInvocation) {
+          isFirstInvocation = false;
+          throw exception;
+        }
+      });
+      final result = await commandRunner.run(['--version']);
+      expect(result, equals(ExitCode.usage.code));
+      verify(() => logger.err(exception.message)).called(1);
+      verify(() => logger.info(commandRunner.usage)).called(1);
+    });
+
+    test('handles UsageException', () async {
+      final exception = UsageException('oops!', 'exception usage');
+      var isFirstInvocation = true;
+      when(() => logger.info(any())).thenAnswer((_) {
+        if (isFirstInvocation) {
+          isFirstInvocation = false;
+          throw exception;
+        }
+      });
+      final result = await commandRunner.run(['--version']);
+      expect(result, equals(ExitCode.usage.code));
+      verify(() => logger.err(exception.message)).called(1);
+      verify(() => logger.info('exception usage')).called(1);
+    });
+
+    group('--version', () {
+      test('outputs current version', () async {
+        final result = await commandRunner.run(['--version']);
+        expect(result, equals(ExitCode.success.code));
+        verify(() => logger.info(packageVersion)).called(1);
+      });
+    });
+
+    group('--verbose', () {
+      test('enables verbose logging', () async {
+        final result = await commandRunner.run(['--verbose']);
+        expect(result, equals(ExitCode.success.code));
+
+        verify(() => logger.detail('Argument information:')).called(1);
+        verify(() => logger.detail('  Top level options:')).called(1);
+        verify(() => logger.detail('  - verbose: true')).called(1);
+        verifyNever(() => logger.detail('    Command options:'));
+      });
+
+      test('enables verbose logging for sub commands', () async {
+        final result = await commandRunner.run([
+          '--verbose',
+          'sample',
+          '--cyan',
+        ]);
+        expect(result, equals(ExitCode.success.code));
+
+        verify(() => logger.detail('Argument information:')).called(1);
+        verify(() => logger.detail('  Top level options:')).called(1);
+        verify(() => logger.detail('  - verbose: true')).called(1);
+        verify(() => logger.detail('  Command: sample')).called(1);
+        verify(() => logger.detail('    Command options:')).called(1);
+        verify(() => logger.detail('    - cyan: true')).called(1);
+      });
+    });
+  });
+}
