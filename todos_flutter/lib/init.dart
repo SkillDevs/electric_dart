d8a4540f80b9d37b62b6cd73f56850f039450ddc
David Martos <davidmartos96@gmail.com>
2023-09-20 17:46:17 +0200

    example: provide an option to delete the local database in case of conflict with the server database

--- todos_flutter/lib/init.dart
+++ todos_flutter/lib/init.dart
@@ -1,8 +1,16 @@
+import 'package:electricsql/util.dart';
+import 'package:electricsql_flutter/drivers/drift.dart';
 import 'package:electricsql_flutter/electricsql_flutter.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:todos_electrified/database/database.dart';
 import 'package:todos_electrified/database/drift/database.dart';
 import 'package:todos_electrified/electric.dart';
+import 'package:todos_electrified/theme.dart';
+import 'package:todos_electrified/util/confirmation_dialog.dart';
+import 'package:todos_electrified/database/drift/connection/connection.dart'
+    as impl;
 
 typedef InitData = ({
   TodosDatabase todosDb,
@@ -10,8 +18,10 @@ typedef InitData = ({
   ConnectivityStateController connectivityStateController,
 });
 
-InitData? useInitData() {
-  final initDataVN = useState<InitData?>(null);
+void useInitializeApp(ValueNotifier<InitData?> initDataVN) {
+  final retryVN = useState(0);
+
+  final context = useContext();
 
   useEffect(() {
     bool mounted = true;
@@ -20,13 +30,41 @@ InitData? useInitData() {
     Future<void> init() async {
       final driftRepo = await initDriftTodosDatabase();
       if (!mounted) return;
+
       final todosDb = TodosDatabase(driftRepo);
       // final sqliteRepo = initSqliteRepository(dbPath);
       // final todosDb = TodosDatabase(sqliteRepo);
       // final adapter = SqliteAdapter(sqliteRepo.db);
 
       const dbName = "todos_db";
-      final electricClient = await startElectricDrift(dbName, driftRepo.db);
+
+      final DriftElectricClient<AppDatabase> electricClient;
+      try {
+        electricClient = await startElectricDrift(dbName, driftRepo.db);
+      } on SatelliteException catch (e) {
+        if (mounted) {
+          if (!kIsWeb && e.code == SatelliteErrorCode.unknownSchemaVersion) {
+            // Ask to delete the database
+            final shouldDeleteLocal = await launchConfirmationDialog(
+              title: "Local schema doesn't match server's",
+              content: const Text("Delete local state and retry?"),
+              context: context,
+            );
+
+            if (shouldDeleteLocal == true) {
+              await driftRepo.close();
+
+              await impl.deleteTodosDbFile();
+
+              retryVN.value++;
+              return;
+            }
+          }
+        }
+
+        rethrow;
+      }
+
       if (!mounted) return;
 
       final connectivityStateController =
@@ -44,12 +82,46 @@ InitData? useInitData() {
 
     return () {
       mounted = false;
-
-      initData?.connectivityStateController.dispose();
-      initData?.electricClient.dispose();
     };
-  }, []);
+  }, [retryVN.value]);
+}
 
-  final initData = initDataVN.value;
-  return initData;
+class InitAppLoader extends HookWidget {
+  final ValueNotifier<InitData?> initDataVN;
+
+  const InitAppLoader({required this.initDataVN});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.from(
+        useMaterial3: true,
+        colorScheme: kElectricColorScheme,
+      ),
+      home: HookBuilder(builder: (context) {
+        useInitializeApp(initDataVN);
+
+        return Scaffold(
+          body: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Initializing the app...",
+                    style: Theme.of(context).textTheme.displaySmall,
+                  ),
+                  const SizedBox(height: 20),
+                  const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      }),
+    );
+  }
 }
