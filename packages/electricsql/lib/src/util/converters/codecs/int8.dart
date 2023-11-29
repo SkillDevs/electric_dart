0c9dde0fa85bd5efa56055b0661320daf4c7b7d1
David Martos <davidmartos96@gmail.com>
2023-11-29 19:24:40 +0100

    fix int8 compilation on web

--- packages/electricsql/lib/src/util/converters/codecs/int8.dart
+++ packages/electricsql/lib/src/util/converters/codecs/int8.dart
@@ -3,12 +3,8 @@ import 'package:electricsql/src/util/converters/type_converters.dart';
 final class Int8Codec extends ValidationCodec<int> {
   const Int8Codec() : super(_validate);
 
-  static const int _min = -9223372036854775808;
-  static const int _max = 9223372036854775807;
-
   static void _validate(int value) {
-    if (value < _min || value > _max) {
-      throw RangeError.range(value, _min, _max);
-    }
+    // Nothing to validate. Int in Dart VM is at most 64 bits.
+    // On Dart JS, the limited integer value also fits
   }
 }
