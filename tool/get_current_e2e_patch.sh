4d466f341d72c1bf3940017bdfc9238bb8dfef27
David Martos <davidmartos96@gmail.com>
2024-04-25 10:43:01 +0200

    Update build e2e patch script to generate always the same date

--- tool/get_current_e2e_patch.sh
+++ tool/get_current_e2e_patch.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 ROOT_DIR=$(realpath .)
 ELECTRIC_REPO_E2E_PATH="$ROOT_DIR"/e2e/electric_repo/e2e
 DART_REPO_E2E_PATH="$ROOT_DIR"/e2e
+
+# Set the timezone to UTC to be consistent in the patch file
+export TZ=UTC
 
 ELECTRIC_COMMIT=$(tool/extract_electric_commit.sh)
 
@@ -21,8 +24,12 @@ mkdir diff
 PATCH_PATH="$ROOT_DIR"/patch/e2e.patch
 
 pushd diff
-ln -s "$ELECTRIC_REPO_E2E_PATH" electric
-ln -s "$DART_REPO_E2E_PATH" dart
+cp -r "$ELECTRIC_REPO_E2E_PATH" electric
+cp -r "$DART_REPO_E2E_PATH" dart
+
+# Update the modified date of all files to be diffed, so that the dates remain
+# the same in the git history
+find . -exec touch -m -d '1/1/2024' {} +
 
 rm "$PATCH_PATH" || true
 diff -x "lux" -x "satellite_client" -x "lux_logs" -ur electric dart > "$PATCH_PATH" || true
