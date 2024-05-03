d628b1a746ba5e132229c37830b95025cf81d621
David Martos <davidmartos96@gmail.com>
2024-05-03 20:10:01 +0200

    Update ping ticker

--- packages/electricsql/lib/src/sockets/io.dart
+++ packages/electricsql/lib/src/sockets/io.dart
@@ -2,10 +2,11 @@ import 'package:electricsql/src/sockets/sockets.dart';
 import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
-/// The interval at which the client ping the server.
+/// The interval at which the client pings the server.
 /// Needed so that Electric doesn't close the connection
-/// https://socket.io/docs/v4/server-options/#pinginterval
-const _kSocketsPingInterval = Duration(seconds: 25);
+/// We use the same tick as the Electric service
+/// https://github.com/electric-sql/electric/blob/364b60365bf48bf8c3a746ec9acf0be74ba8ea15/components/electric/lib/electric/satellite/ws_server.ex#L47
+const _kSocketsPingInterval = Duration(seconds: 5);
 
 SocketFactory getDefaultSocketFactory() {
   return WebSocketIOFactory();
