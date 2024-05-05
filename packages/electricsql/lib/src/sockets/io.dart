d88ef04fb52eeded6897f6fcec0b24078bc82b3c
David Martos <davidmartos96@gmail.com>
2024-05-05 19:14:25 +0200

    Revert: Send websocket pings to avoid the Electric service closing the connection Dart websocket implementation already handles the ping-pong protocol. The original issue was that there was a database connection in the main isolate which was blocking, and in return also blocking Dart from sending the ping responses, making Electric think that the client disconnected.

--- packages/electricsql/lib/src/sockets/io.dart
+++ packages/electricsql/lib/src/sockets/io.dart
@@ -2,12 +2,6 @@ import 'package:electricsql/src/sockets/sockets.dart';
 import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
-/// The interval at which the client pings the server.
-/// Needed so that Electric doesn't close the connection
-/// We use the same tick as the Electric service
-/// https://github.com/electric-sql/electric/blob/364b60365bf48bf8c3a746ec9acf0be74ba8ea15/components/electric/lib/electric/satellite/ws_server.ex#L47
-const _kSocketsPingInterval = Duration(seconds: 5);
-
 SocketFactory getDefaultSocketFactory() {
   return WebSocketIOFactory();
 }
@@ -26,7 +20,6 @@ class WebSocketIO extends WebSocketBase {
   WebSocketChannel createSocketChannel(String url) {
     return IOWebSocketChannel.connect(
       url,
-      pingInterval: _kSocketsPingInterval,
       protocols: [protocolVsn],
     );
   }
